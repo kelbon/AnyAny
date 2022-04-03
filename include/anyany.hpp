0280d7fdea8de1ed8b7f2f00314a7ab4c86fdb64
kelbon <kelbonage@gmail.com>
2022-04-03 15:42:12 +0500

    inline for variables

--- include/anyany.hpp
+++ include/anyany.hpp
@@ -39,8 +39,8 @@ using std::hardware_constructive_interference_size;
 using std::hardware_destructive_interference_size;
 #else
 // 64 bytes on x86-64 │ L1_CACHE_BYTES │ L1_CACHE_SHIFT │ __cacheline_aligned │ ...
-constexpr std::size_t hardware_constructive_interference_size = 64;
-constexpr std::size_t hardware_destructive_interference_size = 64;
+constexpr inline std::size_t hardware_constructive_interference_size = 64;
+constexpr inline std::size_t hardware_destructive_interference_size = 64;
 #endif
 
 // TTA == template template argument (just for better reading)
