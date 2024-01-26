5a4316249fa0a4236245073b56e8f3856ee6a533
kelbon <58717435+kelbon@users.noreply.github.com>
2024-01-26 11:15:49 +0100

    fn_example_update (#65)

--- examples/functional_paradigm.hpp
+++ examples/functional_paradigm.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <iostream>
-#include <functional>
 
 #include <anyany/anyany.hpp>
 
@@ -11,23 +10,15 @@
 /// std::function
 /// std::move_only_function (C++23)
 /// std::function_ref (C++23)
-/// Also function supports currying and invoking from tuple, as in functional languages
 ///
-
-namespace example::details {
-
-template <typename... Methods, typename R, typename... Args>
-auto get_function_type(R (*)(Args...)) -> aa::any_with<aa::call<R(Args...)>, Methods...>;
-
-}  // namespace example::details
 
 namespace example {
 
 template <typename Signature, typename... Methods>
-using basic_function = decltype(details::get_function_type<Methods...>((Signature*)0));
+using basic_function = aa::any_with<aa::call<Signature>, Methods...>;
 // similar to std::function, but better
 // * noexcept move
-// * no huge RTTI
+// * no RTTI
 // * customizable alloc and small object optimization buffer size
 // * support for other signatures like R(Args...) const/noexcept (aa::call Method)
 // etc etc
@@ -45,12 +36,11 @@ using function_ref = aa::cref<aa::call<Signature>>;
 // similar to std::any, but better...
 using any = aa::any_with<aa::copy>;
 
-auto example_foo = [](int x, float y, double z) {
+constexpr auto example_foo = [](int x, float y, double z) {
   std::cout << x << y << z << '\n';
-  return 0;
 };
 
-void use_functions() {
+static void use_functions() {
   function_ref<void(int, float, double) const> f_ref = example_foo;
   f_ref(5, 5.f, 5.);
   function<void(int, float, double)> foo = example_foo;
@@ -61,4 +51,3 @@ void use_functions() {
 }
 
 }  // namespace example
-
