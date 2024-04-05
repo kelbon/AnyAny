cfe29a5c66ed3cc1040175cc45a6100273ac684b
kelbon <58717435+kelbon@users.noreply.github.com>
2024-04-05 08:44:42 +0200

    msvc attribute fix (#74)

--- include/anyany/anyany.hpp
+++ include/anyany/anyany.hpp
@@ -32,7 +32,7 @@
 #if __has_cpp_attribute(no_unique_address)
 #define ANYANY_NO_UNIQUE_ADDRESS [[no_unique_address]]
 #elif __has_cpp_attribute(msvc::no_unique_address)
-#define ANYANY_NO_UNIQUE_ADDRESS msvc::no_unique_address
+#define ANYANY_NO_UNIQUE_ADDRESS [[msvc::no_unique_address]]
 #else
 #define ANYANY_NO_UNIQUE_ADDRESS
 #endif
